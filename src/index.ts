cebb83c656ab22eb3c6b61708144cfaf521a6484
quentin <quentinrichard97@gmail.com>
2021-12-27 18:55:36 +0100

    node project initial configuration

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const PORT = 3000;
+const app = express();
+
+app.get("/test", (req, res) => {
+    res.status(200).send("Hello");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
